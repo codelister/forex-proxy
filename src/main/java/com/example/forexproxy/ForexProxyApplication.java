49baa72ac1e9fbfc4f1dee681d5405907e8353cd
codelister <126431264+codelister@users.noreply.github.com>
2023-03-22 23:50:16 -0700

    Initial commit

---
+++ src/main/java/com/example/forexproxy/ForexProxyApplication.java
@@ -0,0 +1,13 @@
+package com.example.forexproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexProxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/forexproxy/ForexProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forexproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
